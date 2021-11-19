c241773bdcb23a0cf7b189c323c870c0069e2f98
0xstormtrooper <0xstormtrooper@protonmail.com>
2021-11-18 21:58:03 -0500

    get past logs

---
+++ src/get-logs.js
@@ -0,0 +1,49 @@
+const fs = require("fs");
+const path = require("path");
+const Web3 = require("web3");
+const IERC20 = require("./IERC20.json");
+const { readFile } = fs.promises;
+
+async function read(filePath) {
+  const buff = await readFile(filePath);
+  return buff.toString();
+}
+
+const BATCH_SIZE = 1000;
+
+async function main() {
+  const writeStream = fs.createWriteStream(path.join(__dirname, "../out.csv"));
+
+  const config = JSON.parse(await read(path.join(__dirname, "../config.json")));
+  console.log(config);
+
+  const web3 = new Web3(config.provider);
+  const contract = new web3.eth.Contract(IERC20, config.address);
+
+  let fromBlock = config.fromBlock;
+  while (fromBlock <= config.toBlock) {
+    const toBlock = Math.min(fromBlock + BATCH_SIZE, config.toBlock);
+    console.log(`--- blocks: ${fromBlock} - ${toBlock} ---`);
+
+    const logs = await contract.getPastEvents("Transfer", {
+      fromBlock: config.fromBlock,
+      toBlock: config.toBlock,
+    });
+
+    for (const log of logs) {
+      const { from, to, amount } = log.returnValues;
+      console.log({
+        from,
+        to,
+        amount,
+      });
+      writeStream.write(`${from},${to},${amount}\n`);
+    }
+
+    fromBlock += BATCH_SIZE + 1;
+  }
+
+  writeStream.end();
+}
+
+main();
